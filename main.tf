a7c20483cb20c8ed784e5aafee6fc2c2fcaf897e
ahmed badawy <ahmed.badawy@idt.net>
2020-08-28 02:09:18 +0200

    terraform fmt

--- main.tf
+++ main.tf
@@ -1,16 +1,16 @@
 module "vpc" {
-  source = "./modules/vpc"
-  AZ_1   = var.AZ_1
-  AZ_2   = var.AZ_2
+  source        = "./modules/vpc"
+  AZ_1          = var.AZ_1
+  AZ_2          = var.AZ_2
   SUBNET_2_CIDR = var.SUBNET_2_CIDR
   SUBNET_1_CIDR = var.SUBNET_1_CIDR
-  VPC_CIDR = var.VPC_CIDR
+  VPC_CIDR      = var.VPC_CIDR
 }
 
 module "eks" {
-  source = "./modules/eks"
+  source     = "./modules/eks"
   min_size   = var.min_size
   max_size   = var.max_size
-  NAME = var.NAME
+  NAME       = var.NAME
   SUBNET_IDS = module.vpc.SUBNET_IDS
 }
--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -74,6 +74,11 @@ resource "aws_iam_role_policy_attachment" "AmazonEC2ContainerRegistryReadOnly" {
   role       = aws_iam_role.eks_nodes.name
 }
 
+resource "aws_iam_role_policy_attachment" "AmazonEC2ContainerRegistryReadOnly" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.eks_nodes.name
+}
+
 resource "aws_eks_node_group" "node" {
   cluster_name    = aws_eks_cluster.aws_eks.name
   node_group_name = "node"
